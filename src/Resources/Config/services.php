3ae69b4a480ea414491ac29dc9ccc759cc39de46
nawar16 <nawarmk98@gmail.com>
2026-08-02 13:12:25 +0300

    config: register attribute and listener into service container

--- src/Resources/Config/services.php
+++ src/Resources/Config/services.php
@@ -1,10 +1,15 @@
 <?php
 
 use Nawar16\LiteFeatureFlagBundle\Checker\FeatureChecker;
+use Nawar16\LiteFeatureFlagBundle\EventListener\FeatureAttributeListener;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services();
     $services->set(FeatureChecker::class)
-        ->arg('$flags', '%feature_flags.flags%');
+        ->arg('$flags', '%lite_feature_flag.flags%');
+    $services->set(FeatureAttributeListener::class)
+        ->arg('$featureChecker', service(FeatureChecker::class))
+        ->tag('kernel.event_listener', ['event' => 'kernel.controller', 'method' => 'onKernelController']);
 };
